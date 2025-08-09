a45a13a8ca68857c4610744423999e7521a47779
beltus78 <beltus78@gmail.com>
2025-08-09 14:38:12 +0500

    🧹 REMOVE DEBUG FUNCTIONS: Stay under Vercel Hobby 12-function limit (4/12 functions)

--- api/deployment-test.js
+++
@@ -1,7 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({
-    message: 'Deployment test v0.0.29',
-    timestamp: new Date().toISOString(),
-    deploymentTime: '2025-01-09 13:55:00'
-  });
-};
